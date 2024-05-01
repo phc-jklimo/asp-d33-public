d98e37b30b8cb03b601e026d11fc30f90c57d07b
Jan Klimo <klimo@ph-consult.sk>
2024-05-02 00:07:35 +0200

    Fixed nav

--- _app/immutable/modules/pages/_layout.ts-1769a398.js
+++
@@ -1,1 +0,0 @@
-import{l}from"../../chunks/_layout-1d6ee5eb.js";import"../../chunks/i18n-svelte-423cf38f.js";export{l as load};
---
+++ _app/immutable/modules/pages/_layout.ts-73f3ba9c.js
@@ -0,0 +1,1 @@
+import{l}from"../../chunks/_layout-80cd8b09.js";import"../../chunks/i18n-svelte-721b3b72.js";export{l as load};
