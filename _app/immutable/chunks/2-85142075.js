cffe071a997d0298bfbff8a3690801019a83a767
Jan Klimo <klimo@ph-consult.sk>
2024-05-01 13:16:30 +0200

    Pre-Launch changes

--- _app/immutable/chunks/2-85142075.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/__lang__/_page.svelte-e39770ae.js";export{t as component};
--- _app/immutable/modules/pages/_layout.ts-adfa08ee.js
+++
@@ -1,1 +0,0 @@
-import{l}from"../../chunks/_layout-ff974f2c.js";import"../../chunks/i18n-svelte-423cf38f.js";export{l as load};
