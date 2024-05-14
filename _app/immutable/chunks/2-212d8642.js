4044751495adec72c2fed1a341955bc71f24e940
Jan <58696251+phc-jklimo@users.noreply.github.com>
2024-05-14 18:24:55 +0200

    Updated deal text (#24)
    * Updated deal text

--- _app/immutable/chunks/2-212d8642.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/__lang__/_page.svelte-3c41a33b.js";export{t as component};
---
+++ _app/immutable/modules/pages/_layout.ts-5ef07ba8.js
@@ -0,0 +1,1 @@
+import{l}from"../../chunks/_layout-36d77faa.js";import"../../chunks/i18n-svelte-721b3b72.js";export{l as load};
